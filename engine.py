72c4fec5a0d11a941311e0e386a2d02cfa1e4bf8
BoinkGit <rmelyle@gmail.com>
2025-04-24 21:45:27 -0400

    increment and decrement

--- engine.py
+++ engine.py
@@ -1,3 +1,4 @@
+print("Ez Engine v0.1.0\n")
 file = open("test.eze", "r")
 lines = file.readlines()
 variables = {}
@@ -6,10 +7,10 @@ file.close()
 error = 0
 
 # function list:
-#   echo: print
-#   file: let
-#   increment ... by: +=
-#   decrement ... by: -=
+#   echo hello: print(hello)
+#   file: ... - ...: let ... =...
+#   increment file ... by ...: ... += ...
+#   decrement file ... by ...: ... -= ...
 
 for line in lines:
     code = line.split()
@@ -20,16 +21,53 @@ for line in lines:
             print(f"The code translator couldn't understand the code. Please replace \"{code[2]}\" with \"-\".")
             error += 1
             break
-        if code[3] in variables:
-            variables[code[1]] = variables[code[3]]
         if code[3][0] in ["\"","\'"]:
             variables[code[1]] = " ".join(code[3:])[1:-1]
+        elif code[3] in variables:
+            variables[code[1]] = variables[code[3]]
         else:
             variables[code[1]] = int(code[3])
+    elif code[0] == "increment":
+        if code[1] != "file":
+            print(f"The code translator couldn't understand the code. Please replace \"{code[1]}\" with \"file\".")
+            error += 1
+            break
+        if code[2] not in variables:
+            print(f"The code translator couldn't find this file. Please replace \"{code[2]}\" with a named file.")
+            error += 1
+            break
+        else:
+            if code[3] != "by":
+                print(f"The code translator couldn't understand the code. Please replace \"{code[3]}\" with \"by\".")
+                error += 1
+                break
+            if code[4] in variables:
+                variables[code[2]] += variables[code[4]]
+            else:
+                variables[code[2]] += int(code[4])
+    elif code[0] == "decrement":
+        if code[1] != "file":
+            print(f"The code translator couldn't understand the code. Please replace \"{code[1]}\" with \"file\".")
+            error += 1
+            break
+        if code[2] not in variables:
+            print(f"The code translator couldn't find this file. Please replace \"{code[2]}\" with a named file.")
+            error += 1
+            break
+        else:
+            if code[3] != "by":
+                print(f"The code translator couldn't understand the code. Please replace \"{code[3]}\" with \"by\".")
+                error += 1
+                break
+            if code[4] in variables:
+                variables[code[2]] -= variables[code[4]]
+            else:
+                variables[code[2]] -= int(code[4])
 
-
-if error >= 1:
+if error == 1:
     print("\nQuit due to error.")
+elif error > 1:
+    print(f"\nHOW DID YOU GET {error} ERRORS?")
 else: print("\nDone!")
 
 print(f"Variables: {variables}")
